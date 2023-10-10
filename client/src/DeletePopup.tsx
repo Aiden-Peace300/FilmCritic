9f2a074d1a5d6cd7ede57d4118ca2922d09a4203
Aiden Peace <aidenpeace300@gmail.com>
2023-10-10 23:32:25 +0000

    Completed Delete popup for desktop

--- client/src/DeletePopup.tsx
+++ client/src/DeletePopup.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './DeletePopup.css';
 
 interface DeleteConfirmationPopupProps {
   onClose: () => void;
@@ -8,15 +9,18 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
   onClose,
 }) => {
   return (
-    <div className="overlay">
-      <div className="popup">
+    <div className="overlay" onClick={onClose}>
+      <div className="popup" onClick={(e) => e.stopPropagation()}>
         <div className="message">
-          Are you sure you would like to delete this Film from your watchlist?
+          Are you sure you would like to <br />
+          delete this Film from your watchlist?
         </div>
-        <button className="button delete">Delete</button>
-        <button className="button cancel" onClick={onClose}>
-          Cancel
-        </button>
+        <div className="buttons-container">
+          <button className="button delete">Delete</button>
+          <button className="button cancel" onClick={onClose}>
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
