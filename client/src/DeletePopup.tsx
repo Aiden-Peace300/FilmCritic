785564c3a0ed581c72dbbe15892ee9b1acbc2d99
Aiden Peace <aidenpeace300@gmail.com>
2023-10-11 00:33:48 +0000

    Added get http method for retrieving idImdb for users Rated Films

--- client/src/DeletePopup.tsx
+++ client/src/DeletePopup.tsx
@@ -3,11 +3,41 @@ import './DeletePopup.css';
 
 interface DeleteConfirmationPopupProps {
   onClose: () => void;
+  idImdb: string | null; // Update the type to accept null
 }
 
 const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
   onClose,
+  idImdb,
 }) => {
+  if (idImdb === null) {
+    return null; // Return null if idImdb is null
+  }
+
+  // Function to handle the deletion when the "Delete" button is clicked
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`/api/watchlist/${idImdb}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete item from watchlist');
+      }
+
+      // Refresh the window
+      window.location.reload();
+
+      // Close the popup after successful deletion
+      onClose();
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
+
   return (
     <div className="overlay" onClick={onClose}>
       <div className="popup" onClick={(e) => e.stopPropagation()}>
@@ -16,7 +46,9 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
           delete this Film from your watchlist?
         </div>
         <div className="buttons-container">
-          <button className="button delete">Delete</button>
+          <button className="button delete" onClick={handleDelete}>
+            Delete
+          </button>
           <button className="button cancel" onClick={onClose}>
             Cancel
           </button>
