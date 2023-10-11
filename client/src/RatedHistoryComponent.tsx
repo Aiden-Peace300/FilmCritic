9768102fae082f1c1f0f41911ae93b3b734cb7d4
Aiden Peace <aidenpeace300@gmail.com>
2023-10-11 06:48:44 +0000

    Corrected li elements in RatedComponent and RecommendedComponent

--- client/src/RatedHistoryComponent.tsx
+++ client/src/RatedHistoryComponent.tsx
@@ -5,11 +5,18 @@ type RatedFilm = {
   userNote: string;
 };
 
+type FilmTitleAndPoster = {
+  title: string;
+  filmPosters: string;
+};
+
 export default function RatedHistoryComponent() {
-  const [ratedFilms, setRatedFilms] = useState<RatedFilm[]>([]);
+  const [ratedFilms, setRatedFilms] = useState<
+    (RatedFilm & FilmTitleAndPoster)[]
+  >([]);
 
   useEffect(() => {
-    const fetchRatedFilms = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch('/api/idImdb/ratedFilms', {
           method: 'GET',
@@ -19,30 +26,87 @@ export default function RatedHistoryComponent() {
         });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          console.error('Network response was not ok');
+          return;
         }
 
         const data = await response.json();
         console.log('rated history:', data);
-        setRatedFilms(data);
+
+        const ratedFilmData = await Promise.all(
+          data.map(async (film) => {
+            const filmData = await fetchFilmPosterAndTitle(film.idImdb);
+            return { ...film, ...filmData };
+          })
+        );
+
+        setRatedFilms(ratedFilmData);
       } catch (error) {
-        console.error('Error fetching rated films:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchRatedFilms();
+    fetchData();
   }, []);
 
+  async function fetchFilmPosterAndTitle(
+    id: string
+  ): Promise<FilmTitleAndPoster | null> {
+    try {
+      // Your existing code for fetching film data
+      const keyParts = ['k_e', 'i6r', 'uv', '0h'];
+
+      const key = keyParts.join('');
+
+      const response = await fetch(
+        `https://imdb-api.com/en/API/Title/${key}/${id}/Trailer,Ratings,`
+      );
+
+      console.log('IMDB API IN fetchFilmPosterAndTitle');
+
+      if (response.status === 404) {
+        console.error('Resource not found (404)');
+        return null; // Return null if no data is found
+      }
+
+      if (!response.ok) {
+        console.error('Failed to fetch data from IMDb API');
+        return null; // Return null if there's an error
+      }
+
+      const responseData = await response.json();
+
+      const filmTitleAndPoster: FilmTitleAndPoster = {
+        title: responseData.title,
+        filmPosters: responseData.image,
+      };
+
+      return filmTitleAndPoster;
+    } catch (error) {
+      console.error('Error:', error);
+      return null; // Return null in case of an error
+    }
+  }
+
   return (
-    <>
+    <div>
       <h2>Rated History:</h2>
       <ul>
         {ratedFilms.map((film) => (
           <li key={film.idImdb}>
             ID: {film.idImdb}, User Note: {film.userNote}
+            {film.filmPosters !== null && (
+              <div>
+                <h3>Title: {film.title}</h3>
+                <img
+                  src={film.filmPosters || ''}
+                  alt={`Film Poster for ${film.idImdb}`}
+                />
+              </div>
+            )}
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
