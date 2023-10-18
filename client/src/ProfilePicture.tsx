4bc7f173b2519bb9046a79dfa0df58c3316bfaa7
Aiden Peace <aidenpeace300@gmail.com>
2023-10-18 00:11:40 +0000

    implemented users Profile bio

--- client/src/ProfilePicture.tsx
+++ client/src/ProfilePicture.tsx
@@ -4,10 +4,26 @@ import './ProfilePicture.css';
 
 export default function ProfileComponent() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
+  const [profileBio, setProfileBio] = useState<string | null>(null);
 
   useEffect(() => {
-    // Fetch the user's profile picture URL from the server when the component mounts
-    fetchUserProfilePicture();
+    try {
+      // Fetch the user's profile picture URL when the component mounts
+      fetchUserProfilePicture();
+
+      // Fetch the username when the component mounts
+      fetchUsername()
+        .then((username) => setUsername(username))
+        .catch((error) => console.error('Error setting username:', error));
+
+      // Fetch the user's profile bio when the component mounts
+      fetchUserBio()
+        .then((bio) => setProfileBio(bio))
+        .catch((error) => console.error('Error setting user bio:', error));
+    } catch (error) {
+      console.error('Error in useEffect:', error);
+    }
   }, []);
 
   const fetchUserProfilePicture = async () => {
@@ -23,9 +39,9 @@ export default function ProfileComponent() {
       if (response.ok) {
         const data = await response.json();
 
-        const imageUrl = data.imageUrl; // Access the imageUrl property
+        const imageUrl = data.imageUrl;
 
-        setImageUrl(imageUrl); // Set imageUrl as a string
+        setImageUrl(imageUrl);
         console.log('data1', data);
         console.log('imageUrl', imageUrl);
       } else {
@@ -35,6 +51,53 @@ export default function ProfileComponent() {
       console.error('Error fetching user profile picture:', error);
     }
   };
+
+  const fetchUserBio = async () => {
+    try {
+      const response = await fetch('/api/userBio', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const userBio = data.profileBio;
+        setProfileBio(userBio);
+        return userBio;
+      } else {
+        console.error('Failed to fetch user bio');
+        return null;
+      }
+    } catch (error) {
+      console.error('Error fetching user bio:', error);
+      return null;
+    }
+  };
+
+  async function fetchUsername() {
+    try {
+      const response = await fetch('/api/username', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+
+      if (response.ok) {
+        const usernameData = await response.json();
+        const userUsername = usernameData.username;
+        return userUsername.toUpperCase();
+      } else {
+        console.error('Failed to fetch user username', response);
+        return null;
+      }
+    } catch (error) {
+      console.error('Error fetching user username:', error);
+      return null;
+    }
+  }
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -74,25 +137,31 @@ export default function ProfileComponent() {
   console.log(imageUrl);
 
   return (
-    <div>
-      <div className="profile-image-container">
-        <img
-          src={imageUrl || placeholder} // You can use a placeholder image here
-          alt="Profile Picture"
-          id="profile-image"
-        />
+    <div className="profile-container">
+      <div className="profile-row">
+        <div className="profile-column profile-image-column">
+          <img
+            src={imageUrl || placeholder}
+            alt="Profile Picture"
+            id="profile-image"
+          />
+        </div>
+        <div className="profile-column user-info-column">
+          <span className="username">{username || 'Loading UserName...'}</span>
+          <p>{profileBio || 'Loading Bio...'}</p>
+          <input
+            type="file"
+            accept="image/*"
+            name="image"
+            id="profilePictureInput"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+          <label htmlFor="profilePictureInput" className="upload-button">
+            Upload
+          </label>
+        </div>
       </div>
-      <input
-        type="file"
-        accept="image/*"
-        name="image"
-        id="profilePictureInput"
-        onChange={handleFileChange}
-        style={{ display: 'none' }}
-      />
-      <label htmlFor="profilePictureInput" className="upload-button">
-        Upload
-      </label>
     </div>
   );
 }
