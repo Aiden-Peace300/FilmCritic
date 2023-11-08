fd2adc1e6a8a201989a8c8e109f9197d0bfe10c0
Aiden Peace <aidenpeace300@gmail.com>
2023-11-08 21:59:07 +0000

    ensuring that the footer anchor links open a new tab instead of current tab

--- client/src/Footer.tsx
+++ client/src/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div style={{ marginTop: '1rem' }}>
           <span>
             &copy;{' '}
-            <a href="https://github.com/Aiden-Peace300" rel="noreferrer">
+            <a href="https://github.com/Aiden-Peace300" target="_blank">
               Aiden Peace
             </a>
             , 2023. All rights reserved.
@@ -15,21 +15,21 @@ export default function Footer() {
         </div>
         <div className="footer">
           <div>
-            <a href="https://www.imdb.com/" rel="noreferrer">
+            <a href="https://www.imdb.com/" target="_blank">
               Powered by IMDb
             </a>
           </div>
           <div>
             <a
               href="https://platform.openai.com/docs/introduction"
-              rel="noreferrer">
+              target="_blank">
               Powered by Open AI
             </a>
           </div>
           <div>
             <a
               href="https://rapidapi.com/movie-of-the-night-movie-of-the-night-default/api/streaming-availability/details"
-              rel="noreferrer">
+              target="_blank">
               Powered by Streaming Availability
             </a>
           </div>
