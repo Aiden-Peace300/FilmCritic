221ddff0ceec77fc6b99415df76703c901313e1c
Aiden Peace <aidenpeace300@gmail.com>
2024-08-21 21:26:51 +0000

    mobile styling for signup and register page

--- client/src/RegistrationForm.tsx
+++ client/src/RegistrationForm.tsx
@@ -50,12 +50,12 @@ export default function RegistrationForm() {
           <div className="">
             <div className="d-flex red-backdrop justify-center boarder-radius-1">
               <Link to="/sign-in" className="size link-no-underline">
-                <h1 className="no-margin white">LOGIN</h1>
+                <h1 className="no-margin black">LOGIN</h1>
               </Link>
               <Link
                 to="/register"
                 className="selected-login size link-no-underline">
-                <h1 className="no-margin black">REGISTER</h1>
+                <h1 className="no-margin white">REGISTER</h1>
               </Link>
             </div>
           </div>
